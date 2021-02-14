e6d6aa9a31ec1c72e5422581842cfafae08d7aae
EC2 Default User <ec2-user@ip-172-31-20-172.us-east-2.compute.internal>
2021-02-14 03:47:58 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,15 +1,15 @@
 class Movie < ActiveRecord::Base
-  def self.with_ratings(ratings_list)
+  #def self.with_ratings(ratings_list)
   # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
   #  movies with those ratings
   # if ratings_list is nil, retrieve ALL movies
-    if ratings_list.kind_of?(Array)
-      ratings_list.each do |rating|
-        Movie.find_all_by_rating(rating)
-      end
-    end
-    if ratings_list == nil
-      Movie.all
-    end
-  end
+  #  if ratings_list.kind_of?(Array)
+  #    ratings_list.each do |rating|
+  #      Movie.find_all_by_rating(rating)
+  #    end
+  #  end
+  #  if ratings_list == nil
+  #    Movie.all
+  #  end
+  #end
 end
